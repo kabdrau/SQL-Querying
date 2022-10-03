70157e5f34905b64899407d545839991242921ee
Kuat Abdrau <kabdrau@MacBook-Pro.local>
2022-10-02 21:11:59 -0500

    first commit

---
+++ queries_playstore.sql
@@ -0,0 +1,96 @@
+-- Comments in SQL Start with dash-dash --
+SELECT * FROM analytics LIMIT 5;
+
+-- 1. Find the app with an ID of 1880. --
+SELECT * FROM analytics WHERE id = 1880;
+
+-- 2. Find the ID and app name for all apps that were last updated on August 01, 2018. --
+SELECT id, app_name FROM analytics WHERE last_updated = '2018-08-01';
+
+-- 3. Count the number of apps in each category, e.g. “Family | 1972”. --
+SELECT category, COUNT(*) as num_of_apps FROM analytics
+GROUP BY category;
+
+-- 4. Find the top 5 most-reviewed apps and the number of reviews for each. --
+SELECT app_name, reviews FROM analytics
+ORDER BY reviews DESC
+LIMIT 5;
+
+-- 5. Find the app that has the most reviews with a rating greater than equal to 4.8. --
+SELECT app_name, reviews, rating FROM analytics
+WHERE rating >= 4.8
+ORDER BY reviews DESC
+LIMIT 1;
+
+-- 6. Find the average rating for each category ordered by the highest rated to lowest rated. --
+SELECT category, AVG(rating) AS avg_rating FROM analytics
+GROUP BY category
+ORDER BY avg_rating DESC;
+
+-- 7. Find the name, price, and rating of the most expensive app with a rating that’s less than 3. --
+SELECT app_name, price, rating FROM analytics
+WHERE rating < 3
+ORDER BY price DESC
+LIMIT 1;
+
+-- 8. Find all apps with a min install not exceeding 50, that have a rating. Order your results by highest rated first. --
+SELECT app_name, min_installs, rating FROM analytics
+WHERE min_installs <= 50 AND rating IS NOT null
+ORDER BY rating DESC;
+
+-- 9. Find the names of all apps that are rated less than 3 with at least 10000 reviews. --
+SELECT app_name, rating, reviews FROM analytics
+WHERE rating < 3 AND reviews >= 1000;
+
+-- 10. Find the top 10 most-reviewed apps that cost between 10 cents and a dollar. --
+SELECT app_name, reviews, price FROM analytics
+WHERE price BETWEEN 0.1 AND 1
+ORDER BY reviews DESC
+LIMIT 10;
+
+-- 11. Find the most out of date app. Hint: You don’t need to do it this way, but it’s possible to do with a subquery: --
+SELECT app_name, last_updated FROM analytics
+ORDER BY last_updated
+LIMIT 1;
+
+--12. Find the most expensive app (the query is very similar to #11). --
+SELECT app_name, price FROM analytics
+ORDER BY price DESC
+LIMIT 1;
+
+-- 13. Count all the reviews in the Google Play Store. --
+SELECT SUM(reviews) FROM analytics;
+
+-- 14. Find all the categories that have more than 300 apps in them. --
+SELECT category, COUNT(*) as num_of_apps FROM analytics
+GROUP BY category
+HAVING COUNT(*) >= 300;
+
+-- 15. Find the app that has the highest proportion of min_installs to reviews, among apps that have been installed at least 100,000 times. 
+-- Display the name of the app along with the number of reviews, the min_installs, and the proportion. --
+SELECT app_name, reviews, min_installs, CONCAT(reviews/(min_installs/100), '%') AS proportion FROM analytics
+WHERE min_installs >= 100000
+ORDER BY (reviews/(min_installs/100)) DESC
+LIMIT 1;
+
+-- 16. FS1. Find the name and rating of the top rated apps in each category, among apps that have been installed at least 50,000 times. --
+SELECT app_name, rating, category FROM analytics
+WHERE (rating, category) in (
+    SELECT MAX(rating), category FROM analytics
+    WHERE min_installs >= 50000
+    GROUP BY category)
+ORDER BY category;
+
+-- 17. FS2. Find all the apps that have a name similar to “facebook”. --
+SELECT app_name FROM analytics
+WHERE app_name ILIKE '%facebook%';
+
+-- 18. FS3. Find all the apps that have more than 1 genre. --
+-- https://www.postgresql.org/docs/current/functions-array.html#ARRAY-FUNCTIONS-TABLE --
+SELECT app_name, genres FROM analytics
+WHERE cardinality(genres) >= 2;
+
+-- 19. FS4. Find all the apps that have education as one of their genres. --
+-- https://www.postgresql.org/docs/current/functions-array.html#ARRAY-FUNCTIONS-TABLE --
+SELECT app_name, genres FROM analytics
+WHERE genres @> '{"Education"}';
